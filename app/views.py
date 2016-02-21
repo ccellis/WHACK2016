7404310d12e73a2143808d63b11cf2135a007bb2
Coleman Ellis <coleman.ellis@students.olin.edu>
2016-02-20 20:18:19 -0500

    Reviews have locations now

--- app/views.py
+++ app/views.py
@@ -15,11 +15,43 @@ def index():
 def reviews():
     form = ReviewForm()
     if form.validate_on_submit():
-        flash('Review="%s", Rating=%s' %
-              (form.review.data, str(form.rating.data)))
+        #flash('Review="%s", Rating=%s' %
+              #(form.review.data, str(form.rating.data)))
         r = models.Review(review=form.review.data,score=form.rating.data,location="TESTPLACE")
         db.session.add(r)
         db.session.commit()
         return redirect('/reviews')
     return render_template('reviews.html',
                            form=form)
+
+@app.route('/bates', methods=['GET', 'POST'])
+def bates():
+    form = ReviewForm()
+    if form.validate_on_submit():
+        #flash('Review="%s", Rating=%s' %
+              #(form.review.data, str(form.rating.data)))
+        r = models.Review(review=form.review.data,score=form.rating.data,location="Bates")
+        db.session.add(r)
+        db.session.commit()
+        return redirect('/bates')
+    reviews = models.Review.query.all()[::-1]
+    reviews = filter(lambda review: review.location == "Bates",reviews)
+    return render_template('bates.html',
+                           form=form,
+                           reviews = reviews)
+
+@app.route('/tower', methods=['GET', 'POST'])
+def tower():
+    form = ReviewForm()
+    if form.validate_on_submit():
+        #flash('Review="%s", Rating=%s' %
+              #(form.review.data, str(form.rating.data)))
+        r = models.Review(review=form.review.data,score=form.rating.data,location="Tower")
+        db.session.add(r)
+        db.session.commit()
+        return redirect('/tower')
+    reviews = models.Review.query.all()[::-1]
+    reviews = filter(lambda review: review.location == "Tower",reviews)
+    return render_template('tower.html',
+                           form=form,
+                           reviews = reviews)
